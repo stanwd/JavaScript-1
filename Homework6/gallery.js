8d31fd1e1df24db10d45b966024b4a0aeb2a32c9
Stanwd <st@sochi.com>
2018-08-14 15:53:44 +0300

    галерея к дз к уроку 6 JavaScript уровень 1

---
+++ Homework6/gallery.js
@@ -0,0 +1,25 @@
+﻿function init () {
+    var images = document.getElementsByTagName("img");
+    for(var i = 0; i < images.length; i++) {
+        images[i].onclick = changeBigPicture;
+    }
+
+    function changeBigPicture(eventObj) {
+        var appDiv = document.getElementById("big_picture");
+        appDiv.innerHTML = "";
+        var eventElement = eventObj.target;
+        var imageNameParts = eventElement.id.split("_");
+        var src="img/gallery/big/" + imageNameParts[1] + ".jpg";
+        var imageDomElement = document.createElement("img");
+        imageDomElement.src = src;
+        imageDomElement.onload = function () {
+            appDiv.appendChild(imageDomElement);
+        }
+        imageDomElement.onerror = function () {
+            imageDomElement.src = "img/gallery/big/error.jpg";
+            appDiv.appendChild(imageDomElement);
+        }
+
+    }
+}
+window.onload = init;
